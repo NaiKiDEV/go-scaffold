87485b0de3b0aac45dbc97aad18314acd01dd1ba
NaiKiDEV <pc999111@gmail.com>
2024-07-20 00:36:18 +0300

    chore: add some comments for future reference

--- scaffolder/scaffold.go
+++ scaffolder/scaffold.go
@@ -21,14 +21,16 @@ func Scaffold(c config.Config, baseDir string) error {
 	return nil
 }
 
-// TODO: custom error handling
+// TODO: add custom error handling for readable errors
 func recursivelyGenerateFiles(dir config.Directory, baseDir string) error {
+	// TODO: dir.Name should be injectable {{projectName}}
 	err := os.Mkdir(path.Join(baseDir, dir.Name), 0777)
 	if err != nil {
 		return err
 	}
 
 	for _, file := range dir.Files {
+		// TODO: file.Name should be injectable {{projectName}}
 		if file.Name == "" {
 			return fmt.Errorf("file must have a name")
 		}
@@ -38,7 +40,7 @@ func recursivelyGenerateFiles(dir config.Directory, baseDir string) error {
 			return err
 		}
 
-		// TODO: add custom template variable injection into template
+		// TODO: add custom variable injection into template
 		_, err = createdFile.WriteString(file.Template)
 		if err != nil {
 			return err
